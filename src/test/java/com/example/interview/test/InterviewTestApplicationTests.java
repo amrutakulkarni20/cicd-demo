368a49be478c9853c431d6f85fd938ae51b831c8
Amruta Kulkarni <amruta.kulkarni.20121991@gmail.com>
2024-07-20 18:06:44 +0200

    Commentout test class

--- src/test/java/com/example/interview/test/InterviewTestApplicationTests.java
+++ src/test/java/com/example/interview/test/InterviewTestApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.interview.test;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class InterviewTestApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.interview.test;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class InterviewTestApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
